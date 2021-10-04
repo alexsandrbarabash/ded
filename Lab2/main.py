3189dd0b2c0a0eff0cdd0eec6ef10d4ce3a2d1c5
alexsandrbarabash <sbarabas176@gmail.com>
2021-10-04 21:06:24 +0300

    update Lab2

--- Lab2/main.py
+++ Lab2/main.py
@@ -8,8 +8,9 @@ class Person:
         return self.first_name + ' ' + self.last_name
 
 class Passenger(Person):
-    def __init__(self, first_name, last_name, car_number, seat):
+    def __init__(self, id, first_name, last_name, car_number, seat):
         super().__init__(first_name, last_name)
+        self.id = id
         self.car_number = car_number
         self.seat = seat
 
@@ -31,6 +32,17 @@ class Car:
     @property
     def number(self):
         return self.__number
+    
+    def change_number(self, number):
+        self.__number = number
+        for item in self.__passangers:
+            item.car_number = number
+    
+    def delete_passanger(self, id):
+        for i in range(len(self.__passangers)):
+            if self.__passangers[i-1].id == id:
+                self.__passangers.pop(i-1)
+
 
 class Conductor(Person):
      def __init__(self, first_name, last_name, car:Car):
@@ -41,16 +53,35 @@ class Conductor(Person):
 
      def add_passanger(self, passanger: Passenger):
          self.car.add_passanger(passanger)
+     
+     def delete_passanger(self, passanger: Passenger):
+         self.car.delete_passanger(passanger.id)
 
-car = Car(1)
 
-conductor = Conductor('Dmutro', 'Sergiyovuch', car)
+def main():
+    car = Car(1)
 
-ivan = Passenger('Ivan', 'Ivanovuch', 1, 1)
+    conductor = Conductor('Dmutro', 'Sergiyovuch', car)
 
-olya = Passenger('Olya', 'Berezkina', 1, 2)
+    ivan = Passenger(1, 'Ivan', 'Ivanovuch', 1, 1)
 
-conductor.add_passanger(ivan)
-conductor.add_passanger(olya)
+    olya = Passenger(2, 'Olya', 'Berezkina', 1, 2)
+    
+    print('Додаємо пасажирів') # Додаємо пасажирів через кондуктора
+    conductor.add_passanger(ivan)
+    conductor.add_passanger(olya)
 
-print([f'{k.first_name} {k.last_name}' for k in car.passangers])
+    print([f'{k.full_name}' for k in car.passangers])
+    print([f'{k.full_name} car:{k.car_number}' for k in car.passangers])
+
+    car.change_number(2)
+
+    print([f'{k.full_name} car:{k.car_number}' for k in car.passangers])
+
+    print([f'{k.full_name} id:{k.id}' for k in car.passangers])
+
+    conductor.delete_passanger(olya)
+
+    print([f'{k.full_name} id:{k.id}' for k in car.passangers])
+
+main()
